429c14b2c0aea8350fb1dcf879bbef6171560f93
dennistan95 <dennistan95@gmail.com>
2025-05-12 15:42:20 +0800

    Update golf-booking.user.js

--- golf-booking.user.js
+++ golf-booking.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Golf Booking Script
 // @namespace    http://tampermonkey.net/
-// @version      2.16
+// @version      2.17
 // @description  Runs on golf booking page
 // @author       Dennis Tan
 // @match        https://gateway.keppelclub.com.sg/webclub*
